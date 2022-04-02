7c0419df4b8264e824a0eed2cd344556a7b6f4df
Hong <2493920954@qq.com>
2022-04-03 01:32:08 +0800

    givens QR decomposition

--- eigen_value/QR_Method.cpp
+++ eigen_value/QR_Method.cpp
@@ -4,40 +4,40 @@
 // for sparse matrix, use the Givens transformation
 // for dense matrix, use the Householder transformation
 // this two transformation make a matrix to be a Hessenberg matrix
-Eigen::Matrix4f Givens(Eigen::Matrix4f A);
-
-std::tuple<Eigen::Matrix4f, Eigen::Matrix4f> QR_Method(Eigen::Matrix4f A);
+std::tuple<Eigen::Matrix4f, Eigen::Matrix4f> Givens(Eigen::Matrix4f A);
 
 int main() {
     Eigen::Matrix4f A;
     A << 1, 2, 3, 4,
          4, 5, 6, 7,
-         2, 1, 5, 0, 
-         4, 2, 1, 0; 
-    Eigen::Matrix4f hessenberg = Givens(A);
+         2, 4, 5, 3, 
+         4, 3, 3, 5; 
+    auto [q, r] = Givens(A);
 
-    auto [Q, R] = QR_Method(hessenberg);
-
-    std::cout << Q << std::endl;
-    std::cout << R << std::endl;
+    std::cout << q << std::endl;
+    std::cout << r << std::endl;
+    std::cout << q * r << std::endl;
 }
 
-Eigen::Matrix4f Givens(Eigen::Matrix4f A) {
-    Eigen::Matrix4f g = A;
-    for (int i = 0; i < 4 - 2; i++) {
-        Eigen::Vector4f v = g.col(i);
-        for (int j = 3; j >= 2 + i; j--) {
-            if (v[j] == 0 && v[j-1] == 0) continue;
-            float m = sqrt(v[j] * v[j] + v[j-1] * v[j-1]);
-            float c = v[j-1] / m, s = v[j] / m;
-            v[j-1] = v[j-1] * c + v[j] * s;
-            v[j] = 0;
+std::tuple<Eigen::Matrix4f, Eigen::Matrix4f> Givens(Eigen::Matrix4f A) {
+    Eigen::Matrix4f r = A;
+    Eigen::Matrix4f q = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f I = Eigen::Matrix4f::Identity();
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 4; j++) {
+            Eigen::Vector4f v = r.col(i);
+            float m = sqrt(v[j] * v[j] + v[i] * v[i]);
+            float c = v[i] / m, s = v[j] / m;
+            // v[j-1] = v[j-1] * c + v[j] * s;
+            // v[j] = 0;
+            I(i ,i) = c;
+            I(j, j) = c;
+            I(j, i) = -s;
+            I(i, j) = s;
+            r = I * r;
+            q = I * q;
+            I = Eigen::Matrix4f::Identity();
         }
-        g.col(i) = v;
     }
-    return g;
-}
-
-std::tuple<Eigen::Matrix4f, Eigen::Matrix4f> QR_Method(Eigen::Matrix4f A) {
-
+    return { q.transpose(), r };
 }
